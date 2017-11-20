45c59a4b88d254c30c81ba7e109240f46cd1dcb8
tomm450 <tommy.114@hotmail.it>
2017-11-20 13:22:36 +0100

    Add existing file

--- HS_staz.m
+++ HS_staz.m
@@ -1,3 +1,4 @@
+%% HS
 function [lfun,A,B,xc,yc,dl,N,T,x,theta_G] = HS_staz(xp,yp,U_inf)
 %questo è giusto
 lfun = localfunctions; %restituisce le funzioni locali come globali
@@ -16,10 +17,10 @@ Npan = length(xp)-1; % numero di pannelli
 
 %% Versori normali e tangenti
 [N,T] = NTversors(dx,dy,dl);
- 
+
 %% Angoli globali
 [theta_G] = panels_inclination(xp,yp);
-          
+
 %% Velocità indotte
 % Preallocazione matrici coefficienti
 A = zeros(Npt,Npt);
@@ -31,9 +32,9 @@ for j = 1:Npan
     % Matrice di rotazione per portarci nel sistema pannello
     [R] = rotation_matrix(theta_G(j));
     for k = 1:Npan
-        % Raggi fra estremi pannello j e punti di collocazione k        
+        % Raggi fra estremi pannello j e punti di collocazione k
         % Angoli fra estremi pannello j e punti di collocazione k
-        [r1,r2,dt] = radius_dtheta(xc(k),yc(k),xp(j),yp(j),xp(j+1),yp(j+1),R,j,k);    
+        [r1,r2,dt] = radius_dtheta(xc(k),yc(k),xp(j),yp(j),xp(j+1),yp(j+1),R,j,k);
         % Velocità indotte SORGENTI
         [Us,Vs] = source_induced_speed(r1,r2,dt);
         
@@ -59,90 +60,94 @@ A(:,end) = sum(C,2);
 B(:,end) = sum(D,2);
 
 %% Condizione di Kutta
- A(end,:) = B(1,:)+ B(end,:);
- b(end,1) = -U_inf*(T(:,1)+T(:,end));
+A(end,:) = B(1,:)+ B(end,:);
+b(end,1) = -U_inf*(T(:,1)+T(:,end));
 
- %% Risoluzione sistema lineare
- if nargout == 8 %non è richiesta la risoluzione
+%% Risoluzione sistema lineare
+if nargout == 8 %non è richiesta la risoluzione
     return
 end
 
- x = A\b;
- 
+x = A\b;
+
 end
 
 %****************************************************************************%
-%% subfunction
+% subfunction
 
 function [xc,yc] = collocation_points(xp,yp)
-    xc = 0.5*(xp(2:end)+xp(1:end-1));
-    yc = 0.5*(yp(2:end)+yp(1:end-1));
+xc = 0.5*(xp(2:end)+xp(1:end-1));
+yc = 0.5*(yp(2:end)+yp(1:end-1));
 end
 
 function [dx,dy,dl] = panels_dimensions(xp,yp)
-    dx = xp(2:end)-xp(1:end-1);
-    dy = yp(2:end)-yp(1:end-1);
-    dl = sqrt(dx.^2+dy.^2);
+dx = xp(2:end)-xp(1:end-1);
+dy = yp(2:end)-yp(1:end-1);
+dl = sqrt(dx.^2+dy.^2);
 end
 
 function [N,T] = NTversors(dx,dy,dl)
-    N = [(-dy./dl);
-         (dx./dl)];
-    N=N./sqrt(N(1,:).^2+N(2,:).^2);
-    T = [(dx./dl);
-         (dy./dl)];
-    T=T./sqrt(T(1,:).^2+T(2,:).^2);
+N = [(-dy./dl);
+    (dx./dl)];
+N=N./sqrt(N(1,:).^2+N(2,:).^2);
+T = [(dx./dl);
+    (dy./dl)];
+T=T./sqrt(T(1,:).^2+T(2,:).^2);
 end
 
 function [theta_G] = panels_inclination(xp,yp)
-    theta_G = atan2(yp(2:end)-yp(1:end-1),xp(2:end)-xp(1:end-1));
+theta_G = atan2(yp(2:end)-yp(1:end-1),xp(2:end)-xp(1:end-1));
 end
 
 function [R] = rotation_matrix(theta)
-    R = [cos(theta)  sin(theta)
-        -sin(theta) cos(theta)];
+R = [cos(theta)  sin(theta)
+    -sin(theta) cos(theta)];
 end
 
 function [r1,r2,dt] = radius_dtheta(xc,yc,xp,yp,xp2,yp2,R,varargin)
-    if nargin == 7
-        j=2;k=3;
-    else
-        j = varargin{1};
-        k = varargin{2};
-    end
-    
-    r1_G = [xc-xp yc-yp];
-    r2_G = [xc-xp2 yc-yp2];
-    r1 = R*r1_G';
-    r2 = R*r2_G';
-    
-    t1 = atan2(r1(2),r1(1));
-    t2 = atan2(r2(2),r2(1));
-    dt = t2-t1;
-    if j == k && t2-t1<1e-15
-        dt=t1-t2;
-    end
- 
+if nargin == 7
+    j=2;k=3;
+else
+    j = varargin{1};
+    k = varargin{2};
+end
+
+r1_G = [xc-xp yc-yp];
+r2_G = [xc-xp2 yc-yp2];
+r1 = R*r1_G';
+r2 = R*r2_G';
+
+t1 = atan2(r1(2),r1(1));
+t2 = atan2(r2(2),r2(1));
+dt = t2-t1;
+if j == k && t2-t1<1e-15
+    dt=t1-t2;
+end
+
 end
 
 function [Us,Vs] = source_induced_speed(r1,r2,dt,varargin)
-    if nargin == 3
-        sigma = 1;
-    else 
-        sigma = varargin{1};
-    end
-    
-    Us = -sigma./(2*pi)*log(norm(r2)/norm(r1));
-    Vs = sigma.*(dt)/(2*pi);
+if nargin == 3
+    sigma = 1;
+else
+    sigma = varargin{1};
+end
+
+Us = -sigma./(2*pi)*log(norm(r2)/norm(r1));
+Vs = sigma.*(dt)/(2*pi);
 end
 
 function [Uv,Vv] = vortex_induced_speed(r1,r2,dt,varargin)
-    if nargin == 3
-        gamma = 1;
-    else 
-        gamma = varargin{1};
-    end
-    
-    Uv = -gamma.*(dt)/(2*pi);
-    Vv = -gamma./(2*pi)*log(norm(r2)/norm(r1));
+if nargin == 3
+    gamma = 1;
+else
+    gamma = varargin{1};
 end
+
+Uv = -gamma.*(dt)/(2*pi);
+Vv = -gamma./(2*pi)*log(norm(r2)/norm(r1));
+end
+
+%% NACA GEN
+
+
---
+++ xfoil2matlab.m
@@ -0,0 +1,280 @@
+%% XFOIL
+function [pol,foil] = xfoil2matlab(coord,alpha,Re,Mach,iter,varargin)
+% from Mathworks, modified for suit the case
+
+% Run XFoil and return the results.
+% [polar,foil] = xfoil(coord,alpha,Re,Mach,{extra commands})
+%
+% Xfoil.exe needs to be in the same directory as this m function.
+% For more information on XFoil visit these websites;
+%  http://en.wikipedia.org/wiki/XFOIL
+%  http://web.mit.edu/drela/Public/web/xfoil
+%
+% Inputs:
+%    coord: Normalised foil co-ordinates (n by 2 array, of x & y
+%           from the TE-bottm passed the LE to the TE bottom)
+%           or a filename of the XFoil co-ordinate file
+%           or a NACA 4 or 5 digit descriptor (e.g. 'NACA0012'),
+
+%    alpha: Angle-of-attack, can be a vector for an alpha polar
+%       Re: Reynolds number (use Re=0 for inviscid mode)
+%     Mach: Mach number
+% extra commands: Extra XFoil commands
+%           The extra XFoil commands need to be proper xfoil commands
+%           in a character array. e.g. 'oper/iter 150'
+%
+% The transition criterion Ncrit can be specified using the
+% 'extra commands' option as follows,
+% foil = xfoil('NACA0012',10,1e6,0.2,'oper/vpar n 12')
+%
+%   Situation           Ncrit
+%   -----------------   -----
+%   sailplane           12-14
+%   motorglider         11-13
+%   clean wind tunnel   10-12
+%   average wind tunnel    9 <= standard "e^9 method"
+%   dirty wind tunnel    4-8
+%
+% A flap deflection can be added using the following command,
+% 'gdes flap {xhinge} {yhinge} {flap_defelction} exec'
+%
+% Outputs:
+%  polar: structure with the polar coefficients (alpha,CL,CD,CDp,CM,
+%          Top_Xtr,Bot_Xtr)
+%   foil: stucture with the specific aoa values (s,x,y,UeVinf,
+%          Dstar,Theta,Cf,H,cpx,cp) each column corresponds to a different
+%          angle-of-attack.
+%
+% If there are different sized output arrays for the different incidence
+% angles then they will be stored in a structured array, foil(1),foil(2)...
+%
+% Examples:
+%    % Single AoA with a different number of panels
+%    [pol foil] = xfoil('NACA0012',10,1e6,0.0,'panels n 330')
+%
+%    % Change the maximum number of iterations
+%    [pol foil] = xfoil('NACA0012',5,1e6,0.2,'oper iter 50')
+%
+%    % Deflect the trailing edge by 20deg at 60% chord and run multiple incidence angles
+%    [pol foil] = xfoil('NACA0012',[-5:15],1e6,0.2,'oper iter 150','gdes flap 0.6 0 5 exec')
+%    % Plot the results
+%    figure;
+%    plot(pol.alpha,pol.CL); xlabel('alpha [\circ]'); ylabel('C_L'); title(pol.name);
+%    figure; subplot(3,1,[1 2]);
+%    plot(foil(1).xcp(:,end),foil(1).cp(:,end)); xlabel('x');
+%    ylabel('C_p'); title(sprintf('%s @ %g\\circ',pol.name,foil(1).alpha(end)));
+%    set(gca,'ydir','reverse');
+%    subplot(3,1,3);
+%    I = (foil(1).x(:,end)<=1);
+%    plot(foil(1).x(I,end),foil(1).y(I,end)); xlabel('x');
+%    ylabel('y'); axis('equal');
+%
+
+
+
+% Some default values
+if ~exist('coord','var'), coord = 'NACA0012'; end;
+if ~exist('alpha','var'), alpha = 0;    end;
+if ~exist('Re','var'),    Re = 1e6;      end;
+if ~exist('Mach','var'),  Mach = 0.2;    end;
+
+% default foil name
+foil_name = [mfilename];
+
+% default filenames
+wd = fileparts(which(mfilename)); % working directory, where xfoil.exe needs to be
+fname = [mfilename];
+file_coord= [wd filesep foil_name '.foil'];
+
+% Save coordinates
+if ischar(coord)  % Either a NACA string or a filename
+    if ~isempty(regexpi(coord,'^NACA *[0-9]{4,5}$')), % Check if a NACA string
+        foil_name = coord;
+    else             % Filename supplied
+        % set coord file
+        file_coord = coord;
+    end;
+else
+    coord = fliplr(coord)';
+    % Write foil ordinate file
+    if exist(file_coord,'file'),  delete(file_coord); end;
+    fid = fopen(file_coord,'w');
+    if (fid<=0)
+        error([mfilename ':io'],'Unable to create file %s',file_coord);
+    else
+        fprintf(fid,'%s\n',foil_name);
+        fprintf(fid,'%9.5f   %9.5f\n',coord');
+        fclose(fid);
+    end;
+end;
+
+% Write xfoil command file
+fid = fopen([wd filesep fname '.inp'],'w');
+if (fid<=0)
+    error([mfilename ':io'],'Unable to create xfoil.inp file');
+else
+    if ischar(coord)
+        if ~isempty(regexpi(coord,'^NACA *[0-9]{4,5}$')),  % NACA string supplied
+            fprintf(fid,'naca %s\n',coord(5:end));
+            
+        else  % filename supplied
+            fprintf(fid,'load %s\n',file_coord);
+        end;
+    else % Coordinates supplied, use the default filename
+        fprintf(fid,'load %s\n',file_coord);
+    end;
+    % Extra Xfoil commands
+    for ii = 1:length(varargin)
+        txt = varargin{ii};
+        txt = regexprep(txt,'[ \\\/]+','\n');
+        fprintf(fid,'%s\n\n',txt);
+    end;
+    fprintf(fid,'\n\npane\n');
+    fprintf(fid,'\n\noper\n');
+    % set Remolds and Mach
+    fprintf(fid,'re %g\n',Re);
+    fprintf(fid,'mach %g\n',Mach);
+    fprintf(fid,'iter %g\n',iter);
+    % Switch to viscous mode
+    if (Re>0)
+        fprintf(fid,'visc\n');
+    end;
+    
+    % Polar accumulation
+    fprintf(fid,'pacc\n\n\n');
+    % Xfoil alpha calculations
+    for ii = 1:length(alpha)
+        % Individual output filenames
+        file_dump{ii} = sprintf('%s_a%06.3f_dump.dat',fname,alpha(ii));
+        file_cpwr{ii} = sprintf('%s_a%06.3f_cpwr.dat',fname,alpha(ii));
+        % Commands
+        fprintf(fid,'alfa %g\n',alpha(ii));
+        fprintf(fid,'dump %s\n',file_dump{ii});
+        fprintf(fid,'cpwr %s\n',file_cpwr{ii});
+    end;
+    % Polar output filename
+    file_pwrt = sprintf('%s_pwrt.dat',fname);
+    fprintf(fid,'pwrt\n%s\n',file_pwrt);
+    fprintf(fid,'plis\n');
+    fprintf(fid,'\nquit\n');
+    fclose(fid);
+    
+    % execute xfoil
+    %cmd = sprintf('cd %s && xfoil.exe < xfoil.inp > xfoil.out',wd);
+    cmd = sprintf('cd %s && /home/tom/Downloads/Xfoil/bin/xfoil < xfoil2matlab.inp > xfoil.out',wd);
+    
+    [status,result] = system(cmd);
+    if (status~=0),
+        disp(result);
+        error([mfilename ':system'],'Xfoil execution failed! %s',cmd);
+    end;
+    
+    % Read dump file
+    %    #    s        x        y     Ue/Vinf    Dstar     Theta      Cf       H
+    jj = 0;
+    ind = 1;
+    for ii = 1:length(alpha)
+        jj = jj + 1;
+        
+        fid = fopen([wd filesep file_dump{ii}],'r');
+        if (fid<=0)
+            error([mfilename ':io'],'Unable to read xfoil output file %s',file_dump{ii});
+        else
+            D = textscan(fid,'%f%f%f%f%f%f%f%f%f%f%f%f','Delimiter',' ','MultipleDelimsAsOne',true,'CollectOutput',1,'HeaderLines',1);
+            fclose(fid);
+            delete([wd filesep file_dump{ii}]);
+            % store data
+            if ((jj>1) && (size(D{1},1)~=length(foil(ind).x)) && sum(abs(foil(ind).x(:,1)-size(D{1},1)))>1e-6 )
+                ind = ind + 1;
+                jj = 1;
+            end;
+            foil(ind).s(:,jj) = D{1}(:,1);
+            foil(ind).x(:,jj) = D{1}(:,2);
+            foil(ind).y(:,jj) = D{1}(:,3);
+            foil(ind).UeVinf(:,jj) = D{1}(:,4);
+            foil(ind).Dstar(:,jj) = D{1}(:,5);
+            foil(ind).Theta(:,jj) = D{1}(:,6);
+            foil(ind).Cf(:,jj) = D{1}(:,7);
+            foil(ind).H (:,jj)= D{1}(:,8);
+        end;
+        
+        foil(ind).alpha(1,jj) = alpha(jj);
+        
+        % Read cp file
+        fid = fopen([wd filesep file_cpwr{ii}],'r');
+        if (fid<=0),
+            error([mfilename ':io'],'Unable to read xfoil output file %s',file_cpwr{ii});
+        else
+            C = textscan(fid,'%f%f','Delimiter',' ','MultipleDelimsAsOne',true,'CollectOutput',1,'HeaderLines',1);
+            fclose(fid);
+            delete([wd filesep file_cpwr{ii}]);
+            % store data
+            foil(ind).xcp = C{1}(:,1);
+            foil(ind).cp(:,jj) = C{1}(:,2);
+        end;
+    end;
+    
+    % Read polar file
+    %
+    %       XFOIL         Version 6.96
+    %
+    % Calculated polar for: NACA 0012
+    %
+    % 1 1 Reynolds number fixed          Mach number fixed
+    %
+    % xtrf =   1.000 (top)        1.000 (bottom)
+    % Mach =   0.000     Re =     1.000 e 6     Ncrit =  12.000
+    %
+    %   alpha    CL        CD       CDp       CM     Top_Xtr  Bot_Xtr
+    %  ------ -------- --------- --------- -------- -------- --------
+    
+    if exist([wd filesep file_pwrt],'file') == 0
+        error([mfilename ':io'],'Unable to read xfoil polar file %s',file_pwrt);
+    else
+        % Header
+        % Calculated polar for: NACA 0012
+        %[wd filesep file_pwrt]
+        P = fileread([[wd filesep file_pwrt]]);
+        Prow = strsplit(P,'\n');
+        
+        pol.name = strsplit(Prow{4},':');
+        pol.name = strtrim(pol.name{2});
+        
+        %     % xtrf =   1.000 (top)        1.000 (bottom)
+        temp_str = strsplit(Prow{8},' ');
+        pol.xtrf_top = str2double(temp_str{4});
+        pol.xtrf_bot = str2double(temp_str{6});
+        clear temp_str
+        
+        %     % Mach =   0.000     Re =     1.000 e 6     Ncrit =  12.000
+        temp_str = strsplit(Prow{9},' ');
+        pol.Mach = str2double(temp_str{4});
+        pol.Re   = str2double(temp_str{7})*10^str2double(temp_str{9});
+        pol.Ncrit = str2double(temp_str{12});
+        clear temp_str
+        
+        MAT = nan(size(Prow,2)-13,9);
+        
+        for i = 13:size(Prow,2)-1
+            
+            temp = strsplit(Prow{i},' ');
+            for j = 2:size(temp,2)
+                MAT(i-12,j-1) = str2double(temp{j});
+            end
+            
+        end
+        
+        pol.alpha = MAT(:,1);
+        pol.CL  = MAT(:,2);
+        pol.CD  = MAT(:,3);
+        pol.CDp = MAT(:,4);
+        pol.Cm  = MAT(:,5);
+        pol.Top_xtr = MAT(:,6);
+        pol.Bot_Xtr = MAT(:,7);
+        
+    end;
+    
+    
+end;
+
+end
